77c2993b2469162e6580fb35086d4cc5619e35cd
anoopkarnik <danoopkarnik@gmail.com>
2023-11-09 20:13:36 +0530

    Corrected the redirecting request path

--- main/controllers/Controller.py
+++ main/controllers/Controller.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request, jsonify
 import os
 import logging
+import requests
+
 
 logger = logging.getLogger(__name__)
 
@@ -10,15 +12,15 @@ payload_controller = Blueprint("payload_controller",__name__)
 def health_check():
 	return jsonify({"status":"success"})
 
-@payload_controller.route("/notion",methods=['GET','POST','PATCH'])
-def notion_service():
+@payload_controller.route("/notion/<path:subpath>",methods=['POST','GET','PATCH'])
+def notion_service(subpath):
 	service_port = os.environ.get('NOTION_SERVICE_PORT')
-	service_url = f"http://0.0.0.0:{service_port}"
+	service_url = f"http://0.0.0.0:{service_port}/{subpath}"
 	logger.info(f'Redirecting request to {service_url}')
 	response = requests.request(
 		method=request.method,
 		url=service_url,
-		headers=[key:value for key,value in request.headers if key!='Host'],
+		headers={key:value for key,value in request.headers if key!='Host'},
 		data=request.get_data(),
 		cookies=request.cookies,
 		allow_redirects=False
